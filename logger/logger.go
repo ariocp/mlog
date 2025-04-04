3045c014d39b05a4905d08f9f98cd5a0168cfda6
ariocp <ariocp@rambler.ru>
2025-04-04 04:26:19 +0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   .gitignore
    	modified:   LICENSE
    	modified:   README.md
    	modified:   go.mod
    	modified:   logger/logger.go
    	modified:   mlog/mlog.go

--- logger/logger.go
+++ logger/logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"sync"
@@ -12,14 +13,13 @@ import (
 type LogLevel int8
 
 const (
-	LogLevelDebug   LogLevel = iota // Debug level logs
-	LogLevelInfo                    // Info level logs
-	LogLevelWarning                 // Warning level logs
-	LogLevelError                   // Error level logs
-	LogLevelFatal                   // Fatal level logs
+	LogLevelDebug LogLevel = iota
+	LogLevelInfo
+	LogLevelWarning
+	LogLevelError
+	LogLevelFatal
 )
 
-// String converts LogLevel to its string representation
 func (l LogLevel) String() string {
 	switch l {
 	case LogLevelDebug:
@@ -37,166 +37,180 @@ func (l LogLevel) String() string {
 	}
 }
 
-// Logger defines the logging interface
 type Logger interface {
-	SetLogLevel(lvl LogLevel)
-	SetOutput(filePath string) error
+	SetLogLevel(level LogLevel)
+	SetOutput(filepath string) error
 	Flush() error
+	Close() error
+
+	Log(level LogLevel, args ...interface{})
+	Logf(format string, level LogLevel, args ...interface{})
+
 	Debug(args ...interface{})
 	Debugf(format string, args ...interface{})
+
 	Info(args ...interface{})
 	Infof(format string, args ...interface{})
+
 	Warn(args ...interface{})
 	Warnf(format string, args ...interface{})
+
 	Error(args ...interface{})
 	Errorf(format string, args ...interface{})
+
 	Fatal(args ...interface{})
 	Fatalf(format string, args ...interface{})
-	Log(lvl LogLevel, args ...interface{})
-	Logf(lvl LogLevel, format string, args ...interface{})
 }
 
-// LogExtended is the default implementation of the Logger interface
 type LogExtended struct {
-	mu        sync.RWMutex
-	logger    *log.Logger
-	bufWriter *bufio.Writer
-	logLevel  LogLevel
-	logFile   *os.File
+	bufferWriter *bufio.Writer
+	logFile      *os.File
+	logLevel     LogLevel
+	logger       *log.Logger
+	mu           sync.RWMutex
 }
 
-// New creates a new instance of LogExtended with default settings
 func New() *LogExtended {
-	bufWriter := bufio.NewWriter(os.Stdout)
+	bufferWriter := bufio.NewWriter(os.Stdout)
 	return &LogExtended{
-		logger:    log.New(bufWriter, "", log.Lmsgprefix),
-		bufWriter: bufWriter,
-		logLevel:  LogLevelInfo,
+		logLevel:     LogLevelInfo,
+		bufferWriter: bufferWriter,
+		logger:       log.New(bufferWriter, "", 0),
 	}
 }
 
-// SetLogLevel updates the log level for the logger
-func (l *LogExtended) SetLogLevel(lvl LogLevel) {
+func (l *LogExtended) SetLogLevel(level LogLevel) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
-	l.logLevel = lvl
+	l.logLevel = level
 }
 
-// SetOutput sets the output destination for the logger
-func (l *LogExtended) SetOutput(filePath string) error {
+func (l *LogExtended) SetOutput(filepath string) error {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 
-	if l.bufWriter != nil {
-		l.bufWriter.Flush()
+	if l.bufferWriter != nil {
+		l.bufferWriter.Flush()
 	}
-
-	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		return err
-	}
-
 	if l.logFile != nil {
 		l.logFile.Close()
 	}
 
-	bufWriter := bufio.NewWriter(file)
-	l.logger.SetOutput(bufWriter)
-	l.bufWriter = bufWriter
+	file, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return err
+	}
+
+	multiWriter := io.MultiWriter(file, os.Stdout)
+	bufferWriter := bufio.NewWriter(multiWriter)
+
+	l.logger = log.New(bufferWriter, "", 0)
+	l.bufferWriter = bufferWriter
 	l.logFile = file
 
 	return nil
 }
 
-// Flush flushes any buffered log entries
 func (l *LogExtended) Flush() error {
 	l.mu.Lock()
 	defer l.mu.Unlock()
-	if l.bufWriter != nil {
-		return l.bufWriter.Flush()
+	if l.bufferWriter != nil {
+		err := l.bufferWriter.Flush()
+		if err != nil {
+			return err
+		}
+	}
+	if l.logFile != nil {
+		return l.logFile.Sync()
 	}
 	return nil
 }
 
-// formatMessage formats the log message with timestamp and level
-func (l *LogExtended) formatMessage(lvl LogLevel, args ...interface{}) string {
-	timestamp := time.Now().Format("2006-01-02 15:04:05")
-	return fmt.Sprintf("[%s] [%s] %v", timestamp, lvl.String(), fmt.Sprint(args...))
-}
-
-// formatMessagef formats the log message with timestamp and level using format string
-func (l *LogExtended) formatMessagef(lvl LogLevel, format string, args ...interface{}) string {
-	timestamp := time.Now().Format("2006-01-02 15:04:05")
-	msg := fmt.Sprintf(format, args...)
-	return fmt.Sprintf("[%s] [%s] %s", timestamp, lvl.String(), msg)
-}
-
-// Log logs a message with the given log level if it meets the current logging threshold
-func (l *LogExtended) Log(lvl LogLevel, args ...interface{}) {
+func (l *LogExtended) Close() error {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 
-	if lvl < l.logLevel {
+	var err error
+	if l.bufferWriter != nil {
+		if e := l.bufferWriter.Flush(); e != nil {
+			err = e
+		}
+	}
+	if l.logFile != nil {
+		if e := l.logFile.Close(); e != nil {
+			err = e
+		}
+	}
+	return err
+}
+
+func (l *LogExtended) formatMessage(level LogLevel, args ...interface{}) string {
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	return fmt.Sprintf("[%s] [%s] %v", timestamp, level.String(), fmt.Sprint(args...))
+}
+
+func (l *LogExtended) formatMessagef(format string, level LogLevel, args ...interface{}) string {
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	return fmt.Sprintf("[%s] [%s] %s", timestamp, level.String(), fmt.Sprintf(format, args...))
+}
+
+func (l *LogExtended) Log(level LogLevel, args ...interface{}) {
+	l.mu.RLock()
+
+	if level < l.logLevel {
+		l.mu.RUnlock()
 		return
 	}
 
-	l.logger.Println(l.formatMessage(lvl, args...))
+	l.mu.RUnlock()
 
-	if lvl == LogLevelFatal {
+	l.mu.Lock()
+
+	l.logger.Println(l.formatMessage(level, args...))
+
+	if level == LogLevelFatal {
 		l.Flush()
 		if l.logFile != nil {
 			l.logFile.Close()
 		}
 		os.Exit(1)
 	}
+
+	l.mu.Unlock()
 }
 
-// Logf logs a formatted message with the given log level if it meets the current logging threshold
-func (l *LogExtended) Logf(lvl LogLevel, format string, args ...interface{}) {
-	l.mu.Lock()
-	defer l.mu.Unlock()
+func (l *LogExtended) Logf(format string, level LogLevel, args ...interface{}) {
+	l.mu.RLock()
 
-	if lvl < l.logLevel {
+	if level < l.logLevel {
+		l.mu.RUnlock()
 		return
 	}
 
-	l.logger.Println(l.formatMessagef(lvl, format, args...))
+	l.mu.RUnlock()
 
-	if lvl == LogLevelFatal {
+	l.mu.Lock()
+
+	l.logger.Println(l.formatMessagef(format, level, args...))
+
+	if level == LogLevelFatal {
 		l.Flush()
 		if l.logFile != nil {
 			l.logFile.Close()
 		}
 		os.Exit(1)
 	}
+
+	l.mu.Unlock()
 }
 
-// Debug logs a message at debug level
-func (l *LogExtended) Debug(args ...interface{}) { l.Log(LogLevelDebug, args...) }
-
-// Debugf logs a formatted message at debug level
-func (l *LogExtended) Debugf(format string, args ...interface{}) { l.Logf(LogLevelDebug, format, args...) }
-
-// Info logs a message at info level
-func (l *LogExtended) Info(args ...interface{}) { l.Log(LogLevelInfo, args...) }
-
-// Infof logs a formatted message at info level
-func (l *LogExtended) Infof(format string, args ...interface{}) { l.Logf(LogLevelInfo, format, args...) }
-
-// Warn logs a message at warning level
-func (l *LogExtended) Warn(args ...interface{}) { l.Log(LogLevelWarning, args...) }
-
-// Warnf logs a formatted message at warning level
-func (l *LogExtended) Warnf(format string, args ...interface{}) { l.Logf(LogLevelWarning, format, args...) }
-
-// Error logs a message at error level
-func (l *LogExtended) Error(args ...interface{}) { l.Log(LogLevelError, args...) }
-
-// Errorf logs a formatted message at error level
-func (l *LogExtended) Errorf(format string, args ...interface{}) { l.Logf(LogLevelError, format, args...) }
-
-// Fatal logs a message at fatal level and terminates the program
-func (l *LogExtended) Fatal(args ...interface{}) { l.Log(LogLevelFatal, args...) }
-
-// Fatalf logs a formatted message at fatal level and terminates the program
-func (l *LogExtended) Fatalf(format string, args ...interface{}) { l.Logf(LogLevelFatal, format, args...) }
+func (l *LogExtended) Debug(args ...interface{})  { l.Log(LogLevelDebug, args...) }
+func (l *LogExtended) Debugf(format string, args ...interface{}) { l.Logf(format, LogLevelDebug, args...) }
+func (l *LogExtended) Info(args ...interface{})   { l.Log(LogLevelInfo, args...) }
+func (l *LogExtended) Infof(format string, args ...interface{})  { l.Logf(format, LogLevelInfo, args...) }
+func (l *LogExtended) Warn(args ...interface{})   { l.Log(LogLevelWarning, args...) }
+func (l *LogExtended) Warnf(format string, args ...interface{})  { l.Logf(format, LogLevelWarning, args...) }
+func (l *LogExtended) Error(args ...interface{})  { l.Log(LogLevelError, args...) }
+func (l *LogExtended) Errorf(format string, args ...interface{}) { l.Logf(format, LogLevelError, args...) }
+func (l *LogExtended) Fatal(args ...interface{})  { l.Log(LogLevelFatal, args...) }
+func (l *LogExtended) Fatalf(format string, args ...interface{}) { l.Logf(format, LogLevelFatal, args...) }
--- mlog/mlog.go
+++ mlog/mlog.go
@@ -4,47 +4,68 @@ import "github.com/ariocp/mlog/logger"
 
 var mlog = logger.New()
 
-// SetLogLevel updates the log level for the logger
-func SetLogLevel(lvl logger.LogLevel) { mlog.SetLogLevel(lvl) }
+// SetLogLevel sets the minimum log level for messages to be written
+// Messages with a log level lower than the set level will not be logged
+func SetLogLevel(level logger.LogLevel) { mlog.SetLogLevel(level) }
 
-// SetOutput sets the output destination for the logger
-func SetOutput(filePath string) error { return mlog.SetOutput(filePath) }
+// SetOutput sets a new log file for output. It flushes and closes the previous file if necessary
+// If a file path is specified, it opens the file for appending logs and writes to both the file and stdout
+func SetOutput(filepath string) error { return mlog.SetOutput(filepath) }
 
-// Flush flushes any buffered log entries
+// Flush forces all buffered logs to be written to the output
+// This ensures that all log messages are actually written to the output, including any buffered data
 func Flush() error { return mlog.Flush() }
 
-// Debug logs a message at debug level
+// Close flushes any buffered log entries and closes the log file if one was opened
+// It's important to call this before exiting the program to ensure all logs are written
+func Close() error { return mlog.Close() }
+
+// Log writes a log message at the specified log level
+// It checks if the log level is at or above the current configured log level and writes the message
+func Log(level logger.LogLevel, args ...interface{}) { mlog.Log(level, args...) }
+
+// Logf writes a formatted log message at the specified log level
+// It checks if the log level is at or above the current configured log level and writes the formatted message
+func Logf(format string, level logger.LogLevel, args ...interface{}) {
+	mlog.Logf(format, level, args...)
+}
+
+// Debug logs a debug message at the DEBUG level
+// These messages are typically used for debugging purposes and are only logged when the log level includes DEBUG or lower
 func Debug(args ...interface{}) { mlog.Debug(args...) }
 
-// Debugf logs a formatted message at debug level
+// Debugf logs a formatted debug message at the DEBUG level
+// These messages are typically used for debugging purposes and are only logged when the log level includes DEBUG or lower
 func Debugf(format string, args ...interface{}) { mlog.Debugf(format, args...) }
 
-// Info logs a message at info level
+// Info logs an informational message at the INFO level
+// These messages provide general information about the application's state or execution
 func Info(args ...interface{}) { mlog.Info(args...) }
 
-// Infof logs a formatted message at info level
+// Infof logs a formatted informational message at the INFO level
+// These messages provide general information about the application's state or execution
 func Infof(format string, args ...interface{}) { mlog.Infof(format, args...) }
 
-// Warn logs a message at warning level
+// Warn logs a warning message at the WARNING level
+// These messages indicate a potential issue or something that may require attention but doesn't stop execution
 func Warn(args ...interface{}) { mlog.Warn(args...) }
 
-// Warnf logs a formatted message at warning level
+// Warnf logs a formatted warning message at the WARNING level
+// These messages indicate a potential issue or something that may require attention but doesn't stop execution
 func Warnf(format string, args ...interface{}) { mlog.Warnf(format, args...) }
 
-// Error logs a message at error level
+// Error logs an error message at the ERROR level
+// These messages indicate an error that occurred during execution, typically representing a failure or exception
 func Error(args ...interface{}) { mlog.Error(args...) }
 
-// Errorf logs a formatted message at error level
+// Errorf logs a formatted error message at the ERROR level
+// These messages indicate an error that occurred during execution, typically representing a failure or exception
 func Errorf(format string, args ...interface{}) { mlog.Errorf(format, args...) }
 
-// Fatal logs a message at fatal level and terminates the program
+// Fatal logs a fatal error message at the FATAL level and exits the program
+// This level of logging indicates a critical error that causes the program to terminate immediately
 func Fatal(args ...interface{}) { mlog.Fatal(args...) }
 
-// Fatalf logs a formatted message at fatal level and terminates the program
+// Fatalf logs a formatted fatal error message at the FATAL level and exits the program
+// This level of logging indicates a critical error that causes the program to terminate immediately
 func Fatalf(format string, args ...interface{}) { mlog.Fatalf(format, args...) }
-
-// Log logs a message with the given log level if it meets the current logging threshold
-func Log(lvl logger.LogLevel, args ...interface{}) { mlog.Log(lvl, args...) }
-
-// Logf logs a formatted message with the given log level if it meets the current logging threshold
-func Logf(lvl logger.LogLevel, format string, args ...interface{}) { mlog.Logf(lvl, format, args...) }
